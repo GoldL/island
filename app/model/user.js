43668bde3729cca1fe7af979075eb3afcf8890a5
iGolden <136895062@qq.com>
2019-06-22 13:33:44 +0800

    合并代码没有生效，手动提交model

---
+++ app/model/user.js
@@ -0,0 +1,28 @@
+const {
+  Sequelize,
+  Model
+} = require('sequelize')
+
+const {
+  sequelize
+} = require('../../core/db')
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true, // 主键
+    autoIncrement: true // 自动增长
+  },
+  nickname: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  openid: {
+    type: Sequelize.STRING(64),
+    unique: true // 唯一性
+  }
+}, {
+  sequelize,
+  tableName: 'user' // 重命名
+})
