a2e5ed89cb63ebfc1d58877467010f2e3f300d62
iGolden <136895062@qq.com>
2019-07-11 20:05:55 +0800

    添加classic和flow模型

---
+++ app/model/classic.js
@@ -0,0 +1,54 @@
+const {
+  Sequelize,
+  Model
+} = require('sequelize')
+
+const {
+  sequelize
+} = require('../../core/db')
+
+const classicFields = {
+  image: Sequelize.STRING,
+  content: Sequelize.STRING,
+  pubdate: Sequelize.DATEONLY,
+  fav_nums: Sequelize.INTEGER,
+  title: Sequelize.STRING,
+  type: Sequelize.TINYINT,
+}
+
+class Movie extends Model {
+
+}
+
+Movie.init(classicFields, {
+  sequelize,
+  tableName: 'movie' // 重命名
+})
+
+class Sentence extends Model {
+
+}
+
+Sentence.init(classicFields, {
+  sequelize,
+  tableName: 'sentence' // 重命名
+})
+
+class Music extends Model {
+
+}
+
+const musicFields = Object.assign({
+  url: Sequelize.STRING,
+}, classicFields)
+
+Music.init(musicFields, {
+  sequelize,
+  tableName: 'music' // 重命名
+})
+
+module.exports = {
+  Movie,
+  Sentence,
+  Music
+}
---
+++ app/model/flow.js
@@ -0,0 +1,25 @@
+const {
+  Sequelize,
+  Model
+} = require('sequelize')
+
+const {
+  sequelize
+} = require('../../core/db')
+
+class Flow extends Model {
+
+}
+
+Flow.init({
+  index: Sequelize.INTEGER,
+  artId: Sequelize.INTEGER,
+  type: Sequelize.INTEGER
+}, {
+  sequelize,
+  tableName: 'flow' // 重命名
+})
+
+module.exports = {
+  Flow
+}
